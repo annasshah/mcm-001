bd7c28f82bf5b4940fccf6608b95dc053b879793
annasshah <30356728+annasshah@users.noreply.github.com>
2024-05-28 01:02:22 +0500

    key prop issue resolved

--- components/Select_Dropdown/index.jsx
+++ components/Select_Dropdown/index.jsx
@@ -6,7 +6,7 @@ export const Select_Dropdown = ({options_arr, on_change_handle, required, value=
         <div>
             <Label htmlFor="section" value={label} className='font-bold' />
             <Select value={value}  className='w-full h-auto' onChange={on_change_handle} style={{ backgroundColor: '#D9D9D9' }} id="section" required={required}>
-                {options_arr.map(({value,label,selected, disabled}, ind)=><option disabled={disabled || false} selected={selected || false} value={value}>{label}</option>)}
+                {options_arr.map(({value,label,selected, disabled}, ind)=><option key={ind} disabled={disabled || false} selected={selected || false} value={value}>{label}</option>)}
             </Select>
         </div>
     )
