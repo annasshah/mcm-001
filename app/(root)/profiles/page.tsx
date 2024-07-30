0a8e455fa559da69fe57d79126d5dc1e5402558b
sscodez <ssameershah1200@gmail.com>
2024-07-30 21:05:14 +0500

    Added the code

--- app/(root)/profiles/page.tsx
+++ app/(root)/profiles/page.tsx
@@ -1,12 +1,11 @@
-import ProfileHeader from "../../components/Profile/ProfileHeader";
-import ProfileSection from "../../components/Profile/ProfileSection";
+import ProfileHeader from "../../../components/Profile/ProfileHeader";
+import ProfileSection from "../../../components/Profile/ProfileSection";
 
 const Profiles = () => {
   return (
     <main className="w-full h-full flex-col flex justify-start items-center mt-[80px]">
       <ProfileHeader />
       <ProfileSection />
-
     </main>
   );
 };
