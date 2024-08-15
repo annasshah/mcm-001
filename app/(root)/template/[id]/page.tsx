f815d1d5a3de9a3c155bbb21c245643e5ba26d60
sscodez <ssameershah1200@gmail.com>
2024-08-15 11:21:08 +0500

    Update page.tsx

--- app/(root)/template/[id]/page.tsx
+++ app/(root)/template/[id]/page.tsx
@@ -1,4 +1,3 @@
-// app/page/[id]/page.tsx
 "use client"; // Ensure to use "use client" if using React hooks
 
 import React from "react";
@@ -9,15 +8,13 @@ import { KoalaWelcomeEmail } from "@/components/EmailTemplate/koalatemplate";
 import { StackOverflowTipsEmail } from "@/components/EmailTemplate/stackoverflowTemplate";
 import { YelpRecentLoginEmail } from "@/components/EmailTemplate/yelpTemplate";
 
-export const Page: React.FC = () => {
+function Page() {
   const params = useParams<{ id: string }>();
   const id = params.id as string | undefined; // TypeScript type for id
 
   let ComponentToRender: React.ElementType | null = null;
 
-  switch (
-    parseInt(id ?? "", 10) // Convert id to integer
-  ) {
+  switch (parseInt(id ?? "", 10)) {
     case 1:
       ComponentToRender = VercelInviteUserEmail;
       break;
@@ -34,11 +31,15 @@ export const Page: React.FC = () => {
       ComponentToRender = StackOverflowTipsEmail;
       break;
     default:
-      ComponentToRender = () => <div>Component not found</div>;
+      ComponentToRender = function NotFound() {
+        return <div>Component not found</div>;
+      };
       break;
   }
 
   return <div>{ComponentToRender ? <ComponentToRender /> : null}</div>;
-};
+}
+
 Page.displayName = "Template";
+
 export default Page;
